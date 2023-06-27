3cb1c8cb0adfd507834de0417f14742bd540827d
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-06-27 23:08:32 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class loops{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        sc.close();
+        for ( int i = 1; i<=n; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+    
+}
