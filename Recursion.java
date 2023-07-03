b63408a8a97f54fe536cf092591b160cb231ad82
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-07-03 22:52:42 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,29 @@
+// public class Recursion {
+//     public static void printNumb(int n){
+//         if(n==0){
+//             return;
+//         }
+//         System.out.println(n);
+//         printNumb(n-1);
+//     }
+//     public static void main(String args[]){
+//         int n=5;
+//         printNumb(n);
+//     }
+// }
+
+/* Print the sum of n natural nos */
+public class Recursion{
+    public static void printSum(int i, int n, int sum){
+        if(i==n){
+            sum += i;
+            System.out.print(sum);
+            return;
+        }
+        sum += i;
+        printSum(i+1,n,sum);
+    }
+    public static void main(String args[]){
+        printSum(1,5,0);
+    }
+}
