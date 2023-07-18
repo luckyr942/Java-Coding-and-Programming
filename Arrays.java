1622a192d976647c3000d3ce3d735742b66fc378
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-07-18 21:22:13 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,18 +1,18 @@
-// public class Arrays{
-//     public static void main(String args[]) {
-//         int [] marks = new int[3];
-//         marks[0] = 97; //phy
-//         marks[1] = 98; //chem
-//         marks[2] = 99; //maths
-//         // System.out.println(marks[0]);
-//         // System.out.println(marks[1]);
-//         // System.out.println(marks[2]);
+public class Arrays{
+    public static void main(String args[]) {
+        int [] marks = new int[3];
+        marks[0] = 97; //phy
+        marks[1] = 98; //chem
+        marks[2] = 99; //maths
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
         
-//         for (int i=0; i<3; i++){
-//         System.out.println(marks[i]);
-//         }
-//     }
-// }
+        for (int i=0; i<3; i++){
+        System.out.println(marks[i]);
+        }
+    }
+}
 
 
 //user se input leke store karana
