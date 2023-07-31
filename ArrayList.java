3e30e40a9ca850b8e55ec3ce0a3d8ac8099cfa36
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-07-31 22:29:19 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+
+class ArrayList{
+    public static void main(String args[]){
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add delete get set add element in between size
+        list.add(0);
+        list.remove(3);
+        list.add(1, 1);
+        list.set(0, 5);
+        int size = list.size();
+
+        //loops iteration 
+        for(i=0; i<list.size(); i++){
+            System.out.print(list.get(i));
+        }
+        System.out.println();
+    }
+}
