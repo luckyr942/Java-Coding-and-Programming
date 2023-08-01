359c8a5a32ca7ce4f2190c963fb3ff27680bad77
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-08-01 22:47:12 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,28 @@
+class LinearSearch{  
+    static int search(int arr[], int N, int X)
+    {
+        for(int i=0; i<N; i++){
+            
+            if(arr[i]==X){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4};
+        int N = arr.length;
+        int X = 4;
+        
+        int index = search(arr,N,X);
+        
+        if (index==1){
+        System.out.println("Element is not present in the array");
+        }
+        else{
+            System.out.println("Element is found at position"+index);
+        }
+    }
+    
+}
